35857e80d8d7e30902b2da66f449f09c988f95e4
INDRA\jvjara <jvjara@indracompany.com>
2023-02-06 23:09:22 -0500

    first commit

---
+++ src/main/java/com/example/mydevops/maven/MyDevopsMavenApplication.java
@@ -0,0 +1,13 @@
+package com.example.mydevops.maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDevopsMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDevopsMavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mydevops/maven/MyDevopsMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mydevops.maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDevopsMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
